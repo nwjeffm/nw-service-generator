55b2623347d9b13f4bd00805383436c6bc6f5d42
Jeffrey Mabazza <jeffrey.mabazza@nuworks.ph>
2017-06-05 13:53:20 +0800

    Updated repository config paths

--- src/config/repository.php
+++ src/config/repository.php
@@ -1,20 +1,6 @@
 <?php
 
-/**
- * Directory configuration
- */
-
 return [
-	/**
-	 * This is where you store your Repositories
-	 */
 	'repositories_directory' => 'Repositories',
-	/**
-	 * This is where you store your Services
-	 */
-	'services_directory' => 'Services'
-	/**
-	 * This is where you store your Service Helpers
-	 */
-	'helpers_directory' => 'Support'
+	'services_directory' => 'Services',
 ];
